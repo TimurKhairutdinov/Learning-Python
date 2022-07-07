d3fd3c5b2073256ed094f50fcaa2ed70a99b8043
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-07-07 12:55:13 +0300

    Lections/Lesson001/Example001 Data_Types/ Create "file Data_Types.py" add int,float,str,boolean,list,None
     add int,float,str,boolean,list,None
    
     add int,float,str,boolean,list,None

---
+++ Lections/Lesson001/Example001Data_Types/Data_Types.py
@@ -0,0 +1,21 @@
+
+# Типы данных и переменные
+# int, float, str, boolean, lists, None
+
+a = 12
+b = 1.2
+name = 'Timur'
+bool = False
+list = [1,2,3,4,5]
+value = None
+
+print(f'{a} - {type(a)}')
+print(f'{b} - {type(b)}')
+print(f'{name} - {type(name)}')
+print(f'{bool} - {type(bool)}')
+print(f'{list} - {type(list)}')
+print(f'{value} - {type(value)}')
+
+
+
+
