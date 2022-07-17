9e8452fbc63f611b41ada3e148298636d792b7a2
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-07-17 19:31:21 +0300

    Lec\les02\ Create files.py

---
+++ Lections/Lesson002ModulesFunc/Files.py
@@ -0,0 +1,29 @@
+# Работа с файлами
+#
+
+color = ['Red', 'Green', 'Blue3']
+
+# Создаем переменую и связываем с текстовым файлом.
+data = open('file.txt', 'a')
+
+# Запись данных
+# data.writelines(color)  # Разделителей не будет
+# data.write('\nnumbers = ')
+# data.write('4\n')
+
+# # Закрытие подключения к файлу
+# data.close()
+
+
+# # exit() - позволяет не выполнять код, который написан ниже.
+
+
+# with open('file.txt', 'a') as data:
+#     data.write('numbers4 = ')
+#     data.write('452')
+
+path = 'file.txt'
+data = open(path,'r')
+for line in data:
+    print(line)
+data.close()
