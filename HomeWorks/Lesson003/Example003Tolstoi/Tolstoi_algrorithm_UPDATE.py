44e21d2ca7558b1c87f807c36658a8df124f85c8
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-07-20 15:10:35 +0300

    hw\les03\ Tolstoi algorithm, update: the add Counter function greatly optimized program.

---
+++ HomeWorks/Lesson003/Example003Tolstoi/Tolstoi_algrorithm_UPDATE.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import json
+
+text = ''
+with open('tolstoi.txt', 'r', encoding='utf-8') as data:
+    for line in data:
+        text += ' ' + line.lower() 
+        print(f'text + done {line}')
+print('Open complete!')
+
+ls = text.split()
+
+with open('result.json', 'w', encoding='utf-8') as fh:
+    fh.write(json.dumps(Counter(ls), ensure_ascii=False))
+    print('result complete!')
+
+
