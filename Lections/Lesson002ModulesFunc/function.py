3c1fcb1c35e8ff8eca954c5a0035344278365fbf
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-15 16:05:20 +0300

    Lec\les2\ Functions

---
+++ Lections/Lesson002ModulesFunc/function.py
@@ -0,0 +1,36 @@
+# Функции
+
+import Files as fs
+
+print(fs.color)
+
+# Значения по умолчанию
+
+
+def new_string(symb, count=3):
+    return symb*count
+
+
+print(new_string('!', 5))   # !!!!!
+print(new_string('!'))  # !!!
+print(new_string(5))    # 15
+
+
+def concantenatio(*params):
+    res: str = ""
+    for item in params:
+        res += item
+    return res
+
+
+def concantenatio_int(*params):
+    res: int = 0
+    for item in params:
+        res += item
+    return res
+
+
+print(concantenatio('a', 'b', 'c', 'd', 'e', 'f'))  # abcdef
+print(concantenatio('1', '2', '3', '4', '5', '6'))  # 123456
+print(concantenatio_int(1, 2, 3, 4, 5, 6))  # 21
+
