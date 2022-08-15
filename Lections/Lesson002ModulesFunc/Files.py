783a7e36dfc7950b26faf689fae72a589d90ebad
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-15 16:04:55 +0300

    Lec\les\ Files - Working with files

--- Lections/Lesson002ModulesFunc/Files.py
+++ Lections/Lesson002ModulesFunc/Files.py
@@ -1,5 +1,5 @@
 # Работа с файлами
-#
+
 
 color = ['Red', 'Green', 'Blue3']
 
@@ -7,23 +7,23 @@ color = ['Red', 'Green', 'Blue3']
 data = open('file.txt', 'a')
 
 # Запись данных
-# data.writelines(color)  # Разделителей не будет
-# data.write('\nnumbers = ')
-# data.write('4\n')
+data.writelines(color)  # Разделителей не будет
+data.write('\nnumbers = ')
+data.write('4\n')
 
 # # Закрытие подключения к файлу
-# data.close()
+data.close()
 
 
 # # exit() - позволяет не выполнять код, который написан ниже.
 
 
-# with open('file.txt', 'a') as data:
-#     data.write('numbers4 = ')
-#     data.write('452')
+with open('file.txt', 'a') as data:
+    data.write('numbers4 = ')
+    data.write('452')
 
 path = 'file.txt'
-data = open(path,'r')
+data = open(path, 'r+')
 for line in data:
     print(line)
 data.close()
